cbc92db4841de8b71e78ce0ed9ed6131990f120f
mlemeh <mlemeh@gmail.com>
2023-09-05 07:21:03 +0300

    c0905-0720

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,29 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const str = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+       />
+     </a>
+   </li>`
+}).join(" ");
+
+gallery.insertAdjacentHTML("beforeend", str);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+
