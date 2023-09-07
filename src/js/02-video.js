755ca7dce8d16c4ff252884c2c80fee70b0ce619
mlemeh <mlemeh@gmail.com>
2023-09-07 06:50:43 +0300

    c0907-0650 resolved

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector("iframe")
+
+const player = new Player(iframe);
+
+player.ready().then(function() {
+  let currentTime = 0;
+  if (localStorage.getItem("videoplayer-current-time"))
+    currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+  player.setCurrentTime(currentTime)
+});
+
+function fnTimeupdate() {
+  player.getCurrentTime().then(
+    function (seconds) {
+      localStorage.setItem("videoplayer-current-time",JSON.stringify(seconds));
+    })
+}
+
+const throttled = throttle(fnTimeupdate, 1000)
+
+player.on('timeupdate', throttled);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
+
+const form = document.querySelector(".feedback-form")
+
+let feedbackFormState = {email : "", message : ""} 
+
+if (localStorage.getItem("feedback-form-state")) {
+    feedbackFormState = JSON.parse(localStorage.getItem("feedback-form-state"));
+    form.email.value = feedbackFormState.email;
+    form.message.value = feedbackFormState.message;
+}
+ 
+function handlerInput() {
+    feedbackFormState.email = form.email.value;
+    feedbackFormState.message = form.message.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+}
+
+const handlerThrottleInput = throttle(handlerInput, 500);
+
+function handlerSubmit(e) {
+    e.preventDefault();
+
+    feedbackFormState = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(feedbackFormState);
+
+    feedbackFormState = { email: "", message: "" };
+    form.email.value = feedbackFormState.email;
+    form.message.value = feedbackFormState.message;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+}
+
+form.addEventListener("input", handlerThrottleInput)
+form.addEventListener("submit", handlerSubmit)
 
